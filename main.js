bc315f8f3a45291107d14a7067944fb5203f1668
Luega <martinelliluca90@gmail.com>
2023-04-21 21:37:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+import * as THREE from "three";
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI();
+
+const world = {
+  plane: {
+    width: 5,
+    height: 5,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
+};
+
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 50).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+    array[i + 2] = z + Math.random();
+  }
+}
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+  flatShading: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+  array[i + 2] = z + Math.random();
+}
+scene.add(planeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // planeMesh.rotation.y += 0.01;
+  // planeMesh.rotation.x += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
