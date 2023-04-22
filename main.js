ca280c869dfce7fb22b7d5b9fcc4094b41180636
Luega <martinelliluca90@gmail.com>
2023-04-22 04:30:41 +0200

    added raycaster and hover effect with gsap

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import gsap from "gsap";
 
+// GUI
 const gui = new dat.GUI();
 
 const world = {
@@ -33,8 +35,17 @@ function generatePlane() {
     const z = array[i + 2];
     array[i + 2] = z + Math.random();
   }
+  const colors = [];
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4);
+  }
+  planeMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
 }
 
+// GENERAL
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -52,22 +63,30 @@ document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
 
+// Mesh
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0xff0000,
   side: THREE.DoubleSide,
   flatShading: true,
+  vertexColors: true,
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 const { array } = planeMesh.geometry.attributes.position;
 for (let i = 0; i < array.length; i += 3) {
-  const x = array[i];
-  const y = array[i + 1];
   const z = array[i + 2];
   array[i + 2] = z + Math.random();
 }
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(0, 0.19, 0.4);
+}
+planeMesh.geometry.setAttribute(
+  "color",
+  new THREE.BufferAttribute(new Float32Array(colors), 3)
+);
 scene.add(planeMesh);
 
+// Light
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
@@ -75,13 +94,59 @@ const backlight = new THREE.DirectionalLight(0xffffff, 1);
 backlight.position.set(0, 0, -1);
 scene.add(backlight);
 
+// Raycaster
+const raycaster = new THREE.Raycaster();
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+});
+
 function animate() {
   requestAnimationFrame(animate);
 
-  // planeMesh.rotation.y += 0.01;
-  // planeMesh.rotation.x += 0.01;
-
   renderer.render(scene, camera);
+
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(planeMesh);
+  if (intersects.length > 0) {
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4,
+    };
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1,
+    };
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      onUpdate: () => {
+        const { color } = intersects[0].object.geometry.attributes;
+        // v-1
+        color.setX(intersects[0].face.a, hoverColor.r);
+        color.setY(intersects[0].face.a, hoverColor.g);
+        color.setZ(intersects[0].face.a, hoverColor.b);
+        // v-2
+        color.setX(intersects[0].face.b, hoverColor.r);
+        color.setY(intersects[0].face.b, hoverColor.g);
+        color.setZ(intersects[0].face.b, hoverColor.b);
+        // v-3
+        color.setX(intersects[0].face.c, hoverColor.r);
+        color.setY(intersects[0].face.c, hoverColor.g);
+        color.setZ(intersects[0].face.c, hoverColor.b);
+
+        color.needsUpdate = true;
+      },
+    });
+  }
 }
 
 animate();
